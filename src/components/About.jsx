1e0106d254e5dfad2d46dd8f39df96d52e8549da
Adri Morente <adrian95morente@gmail.com>
2022-09-09 06:44:57 +0200

    small about info view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,29 @@ import ViewLayer from '../ViewLayer';
 const AboutView = () => {
     return (
         <ViewLayer>
-            <Paper>
-                <Typography>Sobre nadie</Typography>
+            <Paper elevation={8} sx={{ m: 1, p: 2, width: 250 }}>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Typography variant="h4">
+                            Proyecto: Lazarillo Admin Frontend
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h5">
+                            Realizado por:
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Typography variant="h5">
+                            Adrián Morente
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h6">
+                            ETSIIT - Universidad de Granada
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Paper>
         </ViewLayer>
     );
